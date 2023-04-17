8d3d296c2bfc4168519db52356b3be9c711e03f7
EC2 Default User <ec2-user@ip-172-31-0-86.ap-northeast-1.compute.internal>
2023-04-17 11:46:20 +0000

    log_in

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,17 +1,18 @@
 class Public::CustomersController < ApplicationController
 
   def show
+    @customer = current_customer
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def withdraw
   end
-  
+
   def unsubscribe
   end
 
