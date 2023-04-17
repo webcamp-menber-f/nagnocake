a02ff49bbc0b75189d67a45e421a660b4adf310c
EC2 Default User <ec2-user@ip-172-31-14-247.ap-northeast-1.compute.internal>
2023-04-17 11:09:35 +0000

    sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  # strong parameterで姓と名の属性(firstNameとlastName)をpermitする
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_f, :last_name_f, :post, :address, :phone])
+  end
 end
+
