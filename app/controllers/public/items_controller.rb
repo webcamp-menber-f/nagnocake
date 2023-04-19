6370f86e22173a2be461f1d0905133a7fe08d28e
EC2 Default User <ec2-user@ip-172-31-0-86.ap-northeast-1.compute.internal>
2023-04-19 06:19:04 +0000

    admin-genre-index

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,6 +2,7 @@ class Public::ItemsController < ApplicationController
   def index
     @items= Item.all
     @goods_genres = GoodsGenre.all
+    @item = Item.find_by(id: params[:id])
   end
 
   def show
--- app/models/goods_genre.rb
+++ app/models/goods_genre.rb
@@ -1,3 +1,3 @@
 class GoodsGenre < ApplicationRecord
-  has_many :item
+  has_many :items
 end
